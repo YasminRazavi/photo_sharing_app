2391be5d4af9d7f70a002b1b40e245f993cc8798
DobHoy <dob.hoy@gmail.com>
2014-06-09 13:22:31 +0100

    ANything

--- photoapp/app/models/photo.rb
+++ photoapp/app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :collection
-  has_many :comments
   attr_accessible :caption, :image, :title, :collection_id, :user_id
+  belongs_to :user, :class_name=> "User", :foreign_key => "user_id"
+  belongs_to :collection, :class_name=> "Collection", :foreign_key => "collection_id"
+  has_many :comments
 end
