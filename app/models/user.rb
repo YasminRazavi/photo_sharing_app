d02fc4c5770b30d0db08c25d0dde96557b81d9df
YasminRazavi <yasmin.razavi@gmail.com>
2014-06-11 16:55:25 +0100

    working on liking shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
-
+  acts_as_voter
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :role, :bio, :avatar
   # attr_accessible :title, :body
