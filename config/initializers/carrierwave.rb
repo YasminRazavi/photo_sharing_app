11dd0bc8e6dfb5706cc7c791fb41cdeb0b16aa98
YasminRazavi <yasmin.razavi@gmail.com>
2014-06-12 16:44:37 +0100

    amazon v2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => '<AKIAIRKFF4GMCYSGNQYQ>',      # required
-    :aws_secret_access_key  => '<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>',        # required
+    :aws_access_key_id      => 'AKIAIRKFF4GMCYSGNQYQ',      # required
+    :aws_secret_access_key  => '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',        # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = '<collect-photo>'               # required
+  config.fog_directory  = 'collect-photo'               # required
   config.fog_public     = true                               # optional, defaults to true
 end
